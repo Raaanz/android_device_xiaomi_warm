ea6ef835dba72337a22eb3d983fd036cd15bf3da
Arian <arian.kulmer@web.de>
2021-11-05 16:44:55 +0100

    warm: Update power HAL extension and use xiaomi-touch to handle dt2w
    Change-Id: I5c65e0df255853eb2b3018a27ceb807c111bc4df

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -9,25 +9,12 @@
 #include <android-base/logging.h>
 #include <sys/ioctl.h>
 
-#define CMD_DATA_BUF_SIZE 256
-#define COMMON_DATA_CMD 0
-#define SELECT_TOUCH_ID 3
 #define SET_CUR_VALUE 0
 #define TOUCH_DOUBLETAP_MODE 14
-#define TOUCH_MAGIC 0x54
+#define TOUCH_MAGIC 't'
+#define TOUCH_IOC_SETMODE _IO(TOUCH_MAGIC, SET_CUR_VALUE)
 #define TOUCH_DEV_PATH "/dev/xiaomi-touch"
 #define TOUCH_ID 0
-
-typedef struct {
-    int8_t   touch_id;
-    uint8_t  cmd;
-    uint16_t mode;
-    uint16_t data_len;
-    int32_t  data_buf[CMD_DATA_BUF_SIZE];
-} touch_data;
-
-#define TOUCH_IOC_COMMON_DATA _IOW(TOUCH_MAGIC, COMMON_DATA_CMD, touch_data)
-#define TOUCH_IOC_SELECT_TOUCH_ID _IOW(TOUCH_MAGIC, SELECT_TOUCH_ID, int)
 
 namespace aidl {
 namespace android {
@@ -51,14 +38,8 @@ bool setDeviceSpecificMode(Mode type, bool enabled) {
     switch (type) {
         case Mode::DOUBLE_TAP_TO_WAKE: {
             int fd = open(TOUCH_DEV_PATH, O_RDWR);
-            ioctl(fd, TOUCH_IOC_SELECT_TOUCH_ID, TOUCH_ID);
-            touch_data data = {};
-            data.touch_id = TOUCH_ID;
-            data.cmd = SET_CUR_VALUE;
-            data.mode = TOUCH_DOUBLETAP_MODE;
-            data.data_len = 1;
-            data.data_buf[0] = enabled ? 1 : 0;
-            ioctl(fd, TOUCH_IOC_COMMON_DATA, &data);
+            int arg[3] = {TOUCH_ID, TOUCH_DOUBLETAP_MODE, enabled ? 1 : 0};
+            ioctl(fd, TOUCH_IOC_SETMODE, &arg);
             close(fd);
             return true;
         }
@@ -71,4 +52,3 @@ bool setDeviceSpecificMode(Mode type, bool enabled) {
 }  // namespace power
 }  // namespace hardware
 }  // namespace android
-}  // namespace aidl
