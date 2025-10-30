213bbe14888f3ff174c8e855c7d3bb7b449bcb1a
AdarshGrewal <adarshgrewal@gmail.com>
2025-10-30 19:14:35 +0530

    warm: Implement dummy system hypsys service
     * this service needs to be running for cit and camera to work
    
    Change-Id: Ica90d7f09f55e8683b17337c0465da4cd1c72a5a

--- device.mk
+++ device.mk
@@ -175,6 +175,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
+# Hypsys
+PRODUCT_PACKAGES += \
+    xiaomi.system.hypsys.common-service
+
 # Hotword Enrollement
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/hotword/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
