6518a08791e4b3f6a1e9ada92640f04bcbf7df31
zaraknyu <zaraknyu@gmail.com>
2026-01-06 18:28:21 +0000

    warm: remove sqlite3 and libqshcamera lib_fixups

--- extract-files.py
+++ extract-files.py
@@ -36,8 +36,6 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
-        'sqlite3',
-        'libqshcamera',
     ): lib_fixup_odm_suffix,
     (
         'vendor.qti.diaghal@1.0',
