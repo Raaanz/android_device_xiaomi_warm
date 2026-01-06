f9ac371a1df22cf5fe66d91d4f4ae178651076f5
zaraknyu <zaraknyu@gmail.com>
2026-01-06 10:32:45 +0000

    warm: update build fingerprint from OS1.0.10.0.UGVINXM
    Signed-off-by: zaraknyu <zaraknyu@gmail.com>

--- lineage_warm.mk
+++ lineage_warm.mk
@@ -11,21 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from peridot device
-$(call inherit-product, device/xiaomi/peridot/device.mk)
+# Inherit from warm device
+$(call inherit-product, device/xiaomi/warm/device.mk)
 
-PRODUCT_NAME := lineage_peridot
-PRODUCT_DEVICE := peridot
+PRODUCT_NAME := lineage_warm
+PRODUCT_DEVICE := warm
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := 24069PC21G
+PRODUCT_MODEL := 24116PCC1I
 
-PRODUCT_SYSTEM_NAME := peridot_global
-PRODUCT_SYSTEM_DEVICE := peridot
+PRODUCT_SYSTEM_NAME := warm_in_global
+PRODUCT_SYSTEM_DEVICE := warm
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="peridot_global-user 15 AQ3A.240912.001 OS2.0.204.0.VNPMIXM release-keys" \
-    BuildFingerprint=POCO/peridot_global/peridot:15/AQ3A.240912.001/OS2.0.204.0.VNPMIXM:user/release-keys \
+    BuildDesc="warm_in_global-user 14 UKQ1.240624.001 OS1.0.10.0.UGVINXM release-keys" \
+    BuildFingerprint=POCO/warm_in_global/warm:14/UKQ1.240624.001/OS1.0.10.0.UGVINXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
