1c0de5c835af37b7185d9afc1ff4255c231da289
zaraknyu <zaraknyu@gmail.com>
2026-01-07 20:15:13 +0000

    warm: rootdir: Switch vendor/bin files to proprietary
    Co-authored-by: lolipuru <banstellazz@gmail.com>
    Signed-off-by: zaraknyu <zaraknyu@gmail.com>

--- device.mk
+++ device.mk
@@ -184,18 +184,6 @@ PRODUCT_PACKAGES += \
     fstab.default
 
 PRODUCT_PACKAGES += \
-    init.class_main.sh \
-    init.kernel.post_boot-memory.sh \
-    init.kernel.post_boot-pitti.sh \
-    init.kernel.post_boot-pitti_4_0.sh \
-    init.kernel.post_boot-pitti_4_1.sh \
-    init.kernel.post_boot-pitti_default_6_2.sh \
-    init.kernel.post_boot.sh \
-    init.qcom.early_boot.sh \
-    init.qcom.post_boot.sh \
-    init.qcom.sh
-
-PRODUCT_PACKAGES += \
     init.qcom.factory.rc \
     init.warm.rc \
     init.qcom.rc \
