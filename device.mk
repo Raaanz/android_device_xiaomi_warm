95b84753a10287321c537487afa0900c71fec080
zaraknyu <zaraknyu@gmail.com>
2026-01-08 20:13:23 +0000

    warm: import system usr idc keylayout from stock
    Co-authored-by: AdarshGrewal <adarshgrewal@gmail.com>

--- device.mk
+++ device.mk
@@ -256,6 +256,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-goodix.idc \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
