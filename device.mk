6afdde8538cc9de56c1dfbfe3673bc887d79e9f1
zaraknyu <zaraknyu@gmail.com>
2026-01-08 18:49:48 +0000

    warm: Import gps.conf configuration
    Co-authored-by: dodyirawan85 <dev.irawans@gmail.com>
    Signed-off-by: zaraknyu <zaraknyu@gmail.com>

--- device.mk
+++ device.mk
@@ -167,6 +167,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
